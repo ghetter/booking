794be280ae8bcbc1a7c752f5811d085226d5298b
ghetter <49289242+ghetter@users.noreply.github.com>
2024-10-18 01:19:50 +0700

    Add files via upload

--- core/static/js/booking/script.js
+++ core/static/js/booking/script.js
@@ -15,64 +15,50 @@ setInterval(updateTime, 1000);
 window.onload = updateTime;
 
 
-// Логика для отображения подробной информации о нужном корпусе
 document.addEventListener('DOMContentLoaded', function() {
-    const campuses = document.querySelectorAll('.campuses__item.campus:not(.campus-disabled)');
-
-    campuses.forEach(campus => {
-        if (campus.classList.contains('campus-selected')) {
-            const selectedCampusId = campus.querySelector('.campus__id').textContent.trim();
-            
-            const selectedDetail = Array.from(document.querySelectorAll('.campus__detailed')).find(detail => 
-                detail.querySelector('.campus__id').textContent.trim() === selectedCampusId
-            );
-
-            if (selectedDetail) {
-                selectedDetail.classList.remove('campus__detailed-hidden');
-            }
-
+        // Для пустых полей активна функция "Забронировать"
+    const scheduleElements = document.querySelectorAll('.table__field');
+    scheduleElements.forEach(field => {
+        if (field.hasChildNodes()) {
+            field.classList.remove('empty');
         } else {
-            const detail = Array.from(document.querySelectorAll('.campus__detailed')).find(detail => 
-                detail.querySelector('.campus__id').textContent.trim() === campus.querySelector('.campus__id').textContent.trim()
-            );
-
-            if (detail) {
-                detail.classList.add('campus__detailed-hidden');
-            }
+            field.classList.add('empty');
+            field.innerHTML += `
+                <a href="#" class="bookNow">
+                    <div class="bookNow__inner">
+                        <img src="img/booking/bookingPlusIcon.svg" alt="">
+                        <p>Забронировать</p>
+                    </div>
+                </a>
+            `;
         }
     });
 
-    campuses.forEach(campus => {
-        campus.addEventListener('click', function() {
-            const selectedCampusId = this.querySelector('.campus__id').textContent.trim();
+    
 
-            if (this.classList.contains('campus-selected')) {
-                const selectedDetail = Array.from(document.querySelectorAll('.campus__detailed')).find(detail => 
-                    detail.querySelector('.campus__id').textContent.trim() === selectedCampusId
-                );
-
-                if (selectedDetail) {
-                    selectedDetail.classList.remove('campus__detailed-hidden');
-                }
-
-            } else {
-                document.querySelectorAll('.campus__detailed').forEach(detail => {
-                    detail.classList.add('campus__detailed-hidden');
-                });
-
-                const selectedDetail = Array.from(document.querySelectorAll('.campus__detailed')).find(detail => 
-                    detail.querySelector('.campus__id').textContent.trim() === selectedCampusId
-                );
-
-                if (selectedDetail) {
-                    selectedDetail.classList.remove('campus__detailed-hidden');
-                }
-
-                campuses.forEach(c => c.classList.remove('campus-selected'));
-                this.classList.add('campus-selected');
-            }
-        });
-    });
+    
 });
 
 // Функционирование поиска аудитории по её номеру: если находится, то подсвечивается этаж и аудитория; иначе - "В корпусе нет данной аудитории"
+
+// Функционирование pop-up окна. 
+// !Версия №1: По нажатию на "Забронировать" открывается всплывающая форма с бронью. В этом окне по нажатию на крестик само окно закрывается.
+// Версия №2: В форме заранее учитывается временной промежуток и день под которые бронируется аудитория
+// Версия №3: Функционирует вместе с б/д
+document.addEventListener('DOMContentLoaded', function() {
+    const bookNowBtns = document.querySelectorAll('.bookNow');
+    const bookingPopForm = document.querySelector('.pop-ups');
+    const closeBtn = document.querySelector('.form__header-CloseBtn');
+    
+    bookNowBtns.forEach(bookNowBtn => {
+        bookNowBtn.addEventListener('click', () => {
+            bookingPopForm.classList.add('active');
+        })
+    });
+
+    closeBtn.addEventListener('click', () => {
+        bookingPopForm.classList.remove('active');
+    });
+
+    
+});
