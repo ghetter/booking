d955b134205adb8ebbc5a8fa973703479a36cec1
sekabeka <a.brezgin@g.nsu.ru>
2024-10-21 10:12:09 +0700

    add cache

--- core/booking/views.py
+++ core/booking/views.py
@@ -1,6 +1,10 @@
+import json
+import pickle
+
 from datetime import datetime, timedelta, date, time
 
 from django.urls import reverse_lazy
+from django.core.cache import cache
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import FormMixin
 
@@ -14,7 +18,15 @@ class CampusListView(ListView):
     model = Campus
     template_name = 'booking/main_func/campus_list.html'
     context_object_name = 'campuses'
-    queryset = Campus.objects.all()
+
+    def get_queryset(self):
+        campuses = cache.get('campuses')
+        if campuses is None:
+            campuses = Campus.objects.all()
+            cache.set('campuses', pickle.dumps(campuses), 60 * 10)
+        else:
+            campuses = pickle.loads(campuses)
+        return campuses
 
     # TODO: прописать отображение кампуса к url главной страницы,
     #  отобразить информацию для Frontend
@@ -26,12 +38,33 @@ class AudienceListView(ListView):
     context_object_name = 'audiences'
 
     def get_queryset(self):
-        campus = Campus.objects.get(id=self.kwargs['campus'])
-        return Audience.objects.filter(campus=campus)
+        campus_id = self.kwargs['campus']
+        campus = cache.get(f'campus:{campus_id}')
+
+        if campus is None:
+            campus = Campus.objects.get(id=campus_id)
+            cache.set(f'campus:{campus_id}', pickle.dumps(campus), 60 * 10)
+        else:
+            campus = pickle.loads(campus)
+
+        audiences = cache.get(f'campus_audiences:{campus_id}')
+        if audiences is None:
+            audiences = Audience.objects.filter(campus=campus)
+            cache.set(f'campus_audiences:{campus_id}', pickle.dumps(audiences), 60 * 10)
+        else:
+            audiences = pickle.loads(audiences)
+
+        return audiences
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        floors = self.object_list.values_list('floor', flat=True).order_by().distinct()
+        campus_id = self.kwargs['campus']
+        floors = cache.get(f'floors:{campus_id}')
+        if floors is None:
+            floors = self.object_list.values_list('floor', flat=True).order_by().distinct()
+            cache.set(f'floors:{campus_id}', pickle.dumps(floors), 60 * 10)
+        else:
+            floors = pickle.loads(floors)
         context['floors'] = floors
         return context
     # TODO: Отобразить информацию для Frontend
@@ -68,19 +101,30 @@ class AudienceDetailView(LessonMixin, FormMixin, DetailView):
         return super().form_valid(form)
 
     def get_object(self, **kwargs):
-        audience = self.kwargs[self.pk_url_kwarg]
-        return Audience.objects.get(pk=audience)
+        audience_id = self.kwargs[self.pk_url_kwarg]
+        audience = cache.get(f'audience:{audience_id}')
+        if audience is None:
+            audience = Audience.objects.get(pk=audience_id)
+            cache.set(f'audience:{audience_id}', pickle.dumps(audience), 60 * 10)
+        else:
+            audience = pickle.loads(audience)
+        return audience
 
         # TODO: Дописать query
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
+        audience_id = self.object.id
         _date = self.get_date_from_query_params(self.request)
-        reservations = self.object.reservations.filter(
-            time_start__week=_date.isocalendar().week,
-            time_end__week=_date.isocalendar().week
-        )
+        reservations = cache.get(f'audience_reservations:{audience_id}')
+        if reservations is None:
+            reservations = self.object.reservations.filter(
+                time_start__week=_date.isocalendar().week,
+                time_end__week=_date.isocalendar().week
+            )
+            cache.set(f'audience_reservations:{audience_id}', pickle.dumps(reservations), 60 * 10)
+        else:
+            reservations = pickle.loads(reservations)
         context['reservations'] = reservations
         context['form'] = self.get_form()
 
